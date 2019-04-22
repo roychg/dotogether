c3fc91f5be34f8ace7b5f84e0eda64948c1b2d27
rc <48433460+roychg@users.noreply.github.com>
2019-04-22 07:44:58 -0700

    init passport/session

--- server.js
+++ server.js
@@ -12,7 +12,41 @@ const initializeServer = async () => {
   let redisClient = null;
   const app = express();
   const http = require("http").Server(app);
+  const passport = require("lib/passport");
+  const session = require("lib/session");
 
+  // MONGO
+  try {
+    console.log("Connecting to Mongo");
+    await db.connect();
+  } catch (error) {
+    console.log('Unable to connect to MongoDB')
+    if (++retryCount < 3) {
+      console.log("Trying to connect to mongodb again in 3 seconds...");
+      setTimeout(initializeServer, 3000);
+      return;
+    }
+    process.exit(1);
+  }
+
+  // REDIS 
+  redisClient = await redis.connect();
+  redisClient.on("connect", () => console.log("Redis Connected"));
+  redisClient.on("error", err => {
+    console.log("Error connecting to redis ", err);
+  });
+
+  app.use(bp.json(), bp.urlencoded({ extended: false }));
+  app.use(session(redisClient));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+
+  // ERROR HANDLER
+  app.use((err, req, res, next) => {
+    console.log("SERVER ERROR HANDLER ", err);
+    return res.status(err.status).json({ ...err });
+  });
   http.listen(PORT, () => console.log(`App is on ${PORT}`));
 }
 
